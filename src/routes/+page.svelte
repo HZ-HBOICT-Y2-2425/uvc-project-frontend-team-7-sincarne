1fda62a6e1bc088589dd8465e8c9029ed287a265
Arison <arison646@gmail.com>
2024-12-10 00:03:51 +0100

    refactor: setting up project structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
-<script>
-// @ts-nocheck
+<script lang="ts">
+
 
     import "../app.css";
 
-    /**
-	 * @param {string} route
-	 */
-    function changePage(route){
+    function changePage(route : string){
         location.href = route;
+        console.log("fetch")
     }
+
+
 </script>
 
 <section class="mx-4 text-slate-100 flex flex-col items-center justify-center space-y-4 sm:space-y-8 md:space-y-12 lg:space-y-14">
@@ -36,9 +36,9 @@
     <button class="hover:text-gray-400 hover:border-gray-400 
         border-slate-100  p-4 sm:px-12 md:px-16 lg:px-32
         text-md sm:text-lg md:text-xl lg:text-2xl 
-        border-2 sm:border-2 lg:border-4 rounded-sm transition-all">
-        
-            REGISTER NOW
+        border-2 sm:border-2 lg:border-4 rounded-sm transition-all" 
+        on:click={()=>{changePage("/user/login")}}>
+        REGISTER NOW
        
     </button>
 
@@ -51,7 +51,7 @@
         border-slate-100  p-4 sm:px-12 md:px-16 lg:px-32
         text-md sm:text-lg md:text-xl lg:text-2xl 
         border-2 sm:border-2 lg:border-4 rounded-sm transition-all" 
-        on:click={changePage("/recipes")}     >
+        on:click={()=>{changePage("/recipes")}}>
             DEMO TEST
         
     </button>
