ba8130639700a0eadeb915d94eda7bbe51efddc3
Georgy Furs <georgy.furs@gmail.com>
2024-12-15 21:50:42 +0100

    chore: fixed footer spacing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,63 +5,38 @@
 
 <section class="bg-cover bg-center bg-fixed min-h-screen flex flex-col" style="background-image: url('/SinCarneBg.png')">
     <header>
-        
         <section class="bg-black bg-opacity-50 text-slate-100 top-0 w-full">
             <nav class="max-w-7xl mx-auto px-2 sm:px-2 md:px-4 lg:px-6 flex items-center justify-between h-16">
                 <section>
                     <a href="/"><img src="/SinCarneLogo.png" alt="SinCarne Logo" class="h-10 w-auto object-contain"/></a>
                 </section>
                 <section class="flex items-center space-x-2 sm:space-x-2 md:space-x-4 lg:space-x-6">
-
                     <a href="/about">
-                        <button class="hover:text-gray-400 border hover:border-gray-400 border-slate-100 rounded-sm transition-all p-2 sm:border lg:border-2">
-                            About 
-                        </button>
+                        <button class="hover:text-gray-400 border hover:border-gray-400 border-slate-100 rounded-sm transition-all p-2 sm:border lg:border-2">About</button>
                     </a>
                     <a href="/"> 
-                        <button class="hover:text-gray-400 border hover:border-gray-400 border-slate-100 rounded-sm transition-all p-2 sm:border lg:border-2">
-                            CO2 
-                        </button> 
+                        <button class="hover:text-gray-400 border hover:border-gray-400 border-slate-100 rounded-sm transition-all p-2 sm:border lg:border-2">CO2</button> 
                     </a>
-
                     <a href="/leaderboard">
-                        <button class="hover:text-gray-400 border hover:border-gray-400 border-slate-100 rounded-sm transition-all p-2 sm:border lg:border-2">
-                            Leaderboard 
-                        </button>   
+                        <button class="hover:text-gray-400 border hover:border-gray-400 border-slate-100 rounded-sm transition-all p-2 sm:border lg:border-2">Leaderboard</button>   
                     </a>
-
                     <a href="/recipesList">
-                        <button class="hover:text-gray-400 border hover:border-gray-400 border-slate-100 rounded-sm transition-all p-2 sm:border lg:border-2">
-                            Recipes
-                        </button>
+                        <button class="hover:text-gray-400 border hover:border-gray-400 border-slate-100 rounded-sm transition-all p-2 sm:border lg:border-2">Recipes</button>
                     </a>
-
                     <a href="/stat">
-                        <button class="hover:text-gray-400 border hover:border-gray-400 border-slate-100 rounded-sm transition-all p-2 sm:border lg:border-2">
-                            Statistics
-                        </button>
+                        <button class="hover:text-gray-400 border hover:border-gray-400 border-slate-100 rounded-sm transition-all p-2 sm:border lg:border-2">Statistics</button>
                     </a>
-
                     <a href="/">
-                        <button class="hover:text-gray-400 border hover:border-gray-400 border-slate-100 rounded-sm transition-all p-2 sm:border lg:border-2">
-                            Achievements 
-                        </button>
+                        <button class="hover:text-gray-400 border hover:border-gray-400 border-slate-100 rounded-sm transition-all p-2 sm:border lg:border-2">Achievements</button>
                     </a>
-
                     <a href="/teams">
-                        <button class="hover:text-gray-400 border hover:border-gray-400 border-slate-100 rounded-sm transition-all p-2 sm:border lg:border-2">
-                            Teams
-                        </button>
+                        <button class="hover:text-gray-400 border hover:border-gray-400 border-slate-100 rounded-sm transition-all p-2 sm:border lg:border-2">Teams</button>
                     </a>
-                   
-
                     <button>
                         <a href="/">
                             <img src="/profile.png" alt="Profile" class="h-8 sm:h-8 md:h-10 lg:h-12 w-auto max-h-12 max-w-12 object-cover"/>
                         </a>
                     </button>
-
-
                 </section>
             </nav>
         </section>
@@ -71,10 +46,11 @@
         {@render children()}
     </main>
 
-    <footer class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-center w-full mt-auto">
+    <footer class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-center w-full mt-auto mt-8">
         <p class="p-2 text-center text-slate-50 font-bold">TOGETHER, WE CAN MAKE A DIFFERENCE</p>
     </footer>
 </section>
+
 
 <style>
 </style>
