5ecfa96478cc3de060b135c6fe2ab6526d2d7d7d
MitHZ <petr0028@hz.nl>
2024-12-16 12:55:56 +0100

    added profile page and milestones

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,11 +17,15 @@
                     </button>
                     <button class="hover:text-gray-400 border hover:border-gray-400 border-slate-100 rounded-sm transition-all p-2 sm:border lg:border-2">
                         <a href="/"> CO2 </a>
-                    </button>    
+                    </button>  
+                    <button class="hover:text-gray-400 border hover:border-gray-400 border-slate-100 rounded-sm transition-all p-2 sm:border lg:border-2">
+                        <a href="/milestones"> Milestones </a>
+                    </button>   
                     <button>
-                        <a href="/">
+                        <a href="/profile">
                             <img src="/profile.png" alt="Profile" class="h-8 sm:h-8 md:h-10 lg:h-12 w-auto max-h-12 max-w-12 object-cover"/>
                         </a>
+                        
                     </button>
                 </section>
             </nav>
